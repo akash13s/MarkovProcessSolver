dc80182dce0ddda276d07221a6bebe8e57d16d66
Akash Kumar Shrivastva <akash.shrivastva@gmail.com>
2023-11-11 19:11:28 -0500

    Basic solver done

--- MarkovProcessSolver.h
+++ MarkovProcessSolver.h
@@ -45,64 +45,7 @@ private:
     double discountFactor;
     int iterations;
     double tolerance;
-    bool maximise;
-
-    void readFileV2(string inputFile) {
-        bool correctInputFormat = true;
-
-        ifstream file(inputFile);
-        string line;
-
-        vector<string> atoms;
-
-        while (getline(file, line)) {
-            removeLeadingAndTrailingWhitespace(line);
-
-            if (line.empty()) {
-                continue;
-            }
-
-            vector<string> v = split(line, ' ');
-
-            for (int i=0; i<v.size(); i++) {
-                vector<string> w = split(v[i], '=');
-                if (w[0].length() > 2 || w[1].length() > 1) {
-                    cout<<"Error in input file: "<<w[0]<<"="<<w[1]<<endl;
-                    correctInputFormat = false;
-                    break;
-                }
-                int row = w[0][0] - '0';
-                int col = w[0][1] - '0';
-                int val = w[1][0] - '0';
-                // perform validation for row/col/val
-                // string atom = getAtomForSquare(row, col, val, false);
-                // atoms.push_back(atom);
-            }
-        }
-    }
-
-    static vector<string> split(string input, char del) {
-        stringstream ss(input);
-        string token;
-        vector<string> tokens;
-
-        while (!ss.eof()) {
-            getline(ss, token, del);
-            removeLeadingAndTrailingWhitespace(token);
-            tokens.push_back(token);
-        }
-
-        return tokens;
-    }
-
-    static void removeLeadingAndTrailingWhitespace(string &input) {
-        if (!input.empty()) {
-            input.erase(input.begin(), input.begin() + input.find_first_not_of(" \t"));
-            input.erase(std::find_if(input.rbegin(), input.rend(), [](int ch) {
-                return !std::isspace(ch);
-            }).base(), input.end());
-        }
-    }
+    bool maximise, correctInputFormat;
 
     void readFile(string inputFile) {
 
@@ -221,6 +164,7 @@ private:
             string node = itr->first;
             if (!adj[node].empty() and prob.find(node)==prob.end()) {
                 prob[node] = vector<double> {1.0};
+                decisionNode[node] = true;
             }
         }
 
@@ -368,7 +312,9 @@ private:
 
     void printPolicyAndValues() {
         for (auto itr = policy.begin(); itr!= policy.end(); itr++) {
-            cout<<itr->first<<" -> "<<itr->second<<endl;
+            if (adj[itr->first].size()>1) {
+                cout<<itr->first<<" -> "<<itr->second<<endl;
+            }
         }
 
         cout<<endl;
@@ -394,13 +340,95 @@ private:
         printPolicyAndValues();
     }
 
+    void readFileV2(string inputFile) {
+        correctInputFormat = true;
+        ifstream file(inputFile);
+        string line;
+
+        while (getline(file, line)) {
+            removeLeadingAndTrailingWhitespace(line);
+
+            if (line.empty() || line[0] == '#') {
+                continue;
+            }
+
+            // parse this line
+            if (line.find('=') != string::npos) {
+                // reward for a node
+                vector<string> nodeReward = split(line, '=');
+
+                reward[nodeReward[0]] = stod(nodeReward[1]);
+            } else if (line.find('%') != string::npos) {
+                // probabilities for a node
+                vector<string> nodeProbability = split(line, '%');
+
+                string probabilities = nodeProbability[1];
+                vector<string > p = split(probabilities, ' ');
+
+                string node = nodeProbability[0];
+
+                double ps = 0.0;
+                for (const string& pr: p) {
+                    prob[node].push_back(stod(pr));
+                    ps += stod(pr);
+                }
+
+                if (prob[node].size()>1 and ps!=1.0) {
+                    correctInputFormat = false;
+                    cout<<"Error in line: "<<line<<endl;
+                    break;
+                }
+            } else if (line.find(':') != string::npos) {
+                // edges for a node
+                vector<string> nodeEdges = split(line, ':');
+
+                if (nodeEdges[1][0]!='[' || nodeEdges[1][nodeEdges[1].size()-1]!=']') {
+                    correctInputFormat = false;
+                    cout<<"Error in line: "<<line<<endl;
+                    break;
+                }
+
+                nodeEdges[1] = nodeEdges[1].substr(1, nodeEdges[1].size()-2);
+                vector<string> neighbors = split(nodeEdges[1], ',');
+
+                string node = nodeEdges[0];
+                for (const string& ne: neighbors) {
+                    adj[node].push_back(ne);
+                }
+            }
+        }
+    }
+
+    static vector<string> split(string input, char del) {
+        stringstream ss(input);
+        string token;
+        vector<string> tokens;
+
+        while (!ss.eof()) {
+            getline(ss, token, del);
+            removeLeadingAndTrailingWhitespace(token);
+            tokens.push_back(token);
+        }
+
+        return tokens;
+    }
+
+    static void removeLeadingAndTrailingWhitespace(string &input) {
+        if (!input.empty()) {
+            input.erase(input.begin(), input.begin() + input.find_first_not_of(" \t"));
+            input.erase(std::find_if(input.rbegin(), input.rend(), [](int ch) {
+                return !std::isspace(ch);
+            }).base(), input.end());
+        }
+    }
+
 public:
     MarkovProcessSolver(ProgramArguments *arguments) {
         this->tolerance = arguments->tolerance;
         this->iterations = arguments->iterations;
         this->maximise = arguments->maximise;
         this->discountFactor = arguments->discountFactor;
-        readFile(arguments->inputFile);
+        readFileV2(arguments->inputFile);
         // initialise policies and rewards
         init();
     }
